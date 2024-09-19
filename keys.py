36af79da55eae3f9347a08591a5241311d39d2bd
siddhantchaudhary036 <160422817+siddhantchaudhary036@users.noreply.github.com>
2024-09-19 13:54:56 -0500

    Create keys.py

---
+++ keys.py
@@ -0,0 +1,19 @@
+# Add your values for the following keys here
+#Email credentials
+smtp_server = 'smtp.gmail.com' # You need not change this either
+smtp_port = 587 # This is the port. You need not change it
+sender_email = '' # This is the email that will send all the below mentioned info
+sender_password = '' # This you will have to create using gmails "App Passwords"
+recipient_email = '' # This is the email which will recieve any error alerts and the daily P&L report
+
+#ICICI Direct credentials
+icici_direct_username = '' #This will likely be your phone number
+icici_direct_totp = '' #You will need to enable 2FA on their website and get the key from there
+icici_direct_pass = '' #This will be the password to login to their website
+
+#Kotak Neo credentials
+kotak_consumer_key = '' #You get this upon signing up for their API
+kotak_consumer_secret = '' #You get this upon signing up for their API
+kotak_userid = '' # Your phone number with country code 
+kotak_password = '' # Your password for the site
+kotak_mpin = '' # Your MPIN for the site
